70ea36bbd761020a2c4cd4622aa5d039872ac224
Brajmohan <brajmohan@indiamart.com>
2023-12-18 22:33:10 +0530

    added new db API to create user after login

--- src/database/databaseApi.js
+++ src/database/databaseApi.js
@@ -1,26 +1,39 @@
-import { databases} from "../appWrite/appwrite";
+import { databases } from "../appWrite/appwrite";
 import { Query } from "appwrite";
 const collectionId = "6570aa637f3e31a920ea";
 const collectionId1 = "6570acd4d7b822e5c1b1";
 
+const databaseID = "656c2c4e3621c2f65000";
+const userCollectionID = "6570a4a1e095e06444d0";
+
 export const databaseApi = {
-  createCompany: async (compName, location, experience, email, address, pfMember, otPayable, panNumber, userId) => {
-    console.log('Collection ID:', collectionId);
+  createCompany: async (
+    compName,
+    location,
+    experience,
+    email,
+    address,
+    pfMember,
+    otPayable,
+    panNumber,
+    userId
+  ) => {
+    console.log("Collection ID:", collectionId);
     try {
       const promise = databases.createDocument(
         "656c2c4e3621c2f65000",
         collectionId,
-        '',
+        "",
         {
-          "compName": compName,
-          "location": location,
-          "experience": experience,
-          "email": email,
-          "address": address,
-          "pfMember": pfMember,
-          "otPayable": otPayable,
-          "panNumber": panNumber
-        },
+          compName: compName,
+          location: location,
+          experience: experience,
+          email: email,
+          address: address,
+          pfMember: pfMember,
+          otPayable: otPayable,
+          panNumber: panNumber,
+        }
       );
 
       const response = await promise;
@@ -30,25 +43,37 @@ export const databaseApi = {
     }
   },
 
-  createEmployee: async (empName, gender, email, mobile, dob, address, doj, panNumber, aadhar, compName, userId) => {
-    console.log('Collection ID:', collectionId);
+  createEmployee: async (
+    empName,
+    gender,
+    email,
+    mobile,
+    dob,
+    address,
+    doj,
+    panNumber,
+    aadhar,
+    compName,
+    userId
+  ) => {
+    console.log("Collection ID:", collectionId);
     try {
       const promise = databases.createDocument(
         "656c2c4e3621c2f65000",
         collectionId1,
-        '',
+        "",
         {
-          "empName": empName,
-          "gender": gender,
-          "email": email,
-          "mobile": mobile,
-          "dob": dob,
-          "address": address,
-          "doj": doj,
-          "panNumber": panNumber,
-          "aadhar": aadhar,
-          "compName": compName
-        },
+          empName: empName,
+          gender: gender,
+          email: email,
+          mobile: mobile,
+          dob: dob,
+          address: address,
+          doj: doj,
+          panNumber: panNumber,
+          aadhar: aadhar,
+          compName: compName,
+        }
       );
 
       const response = await promise;
@@ -60,7 +85,10 @@ export const databaseApi = {
 
   getAllCompanies: async () => {
     try {
-      const promise = databases.listDocuments('656c2c4e3621c2f65000', collectionId);
+      const promise = databases.listDocuments(
+        "656c2c4e3621c2f65000",
+        collectionId
+      );
       const response = await promise;
       return response.documents;
     } catch (error) {
@@ -70,7 +98,10 @@ export const databaseApi = {
 
   getAllEmployees: async () => {
     try {
-      const promise = databases.listDocuments('656c2c4e3621c2f65000', collectionId1);
+      const promise = databases.listDocuments(
+        "656c2c4e3621c2f65000",
+        collectionId1
+      );
       const response = await promise;
       return response.documents;
     } catch (error) {
@@ -80,29 +111,52 @@ export const databaseApi = {
 
   getCompaniesByUserId: async (userId) => {
     try {
-        
     } catch (error) {
       console.log(error); // Failure
     }
   },
-  
 
   getEmployeesByCompanyName: async (compName) => {
-    console.log("Backend compName = ",compName)
+    console.log("Backend compName = ", compName);
     try {
-
-      const promise = databases.listDocuments('656c2c4e3621c2f65000', collectionId1, [Query.equal('compName', compName)]);
+      const promise = databases.listDocuments(
+        "656c2c4e3621c2f65000",
+        collectionId1,
+        [Query.equal("compName", compName)]
+      );
       const response = await promise;
       if (response.error) {
         console.error(response.error);
-        throw new Error('Failed to fetch employees.');
+        throw new Error("Failed to fetch employees.");
       }
       return response.documents;
     } catch (error) {
       console.error(error);
       // Handle the error as needed (e.g., show a message to the user)
-      throw new Error('Failed to fetch employees.');
+      throw new Error("Failed to fetch employees.");
     }
-  }
-};
+  },
 
+  createUser: async (userName, userEmail, uniqueUserID) => {
+    console.log("Collection ID:", collectionId);
+    try {
+      const promise = databases.createDocument(
+        databaseID,
+        userCollectionID,
+        "",
+        {
+          user_name: userName,
+          mobile_number: "",
+          email: userEmail,
+          unique_id: uniqueUserID,
+        }
+      );
+
+      const response = await promise;
+      console.log(response); // Success
+      return response;
+    } catch (error) {
+      console.error(error); // Failure
+    }
+  },
+};

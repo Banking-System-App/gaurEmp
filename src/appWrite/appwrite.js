24c02893302db32ca2e4d8ac025fe4d093aba7b4
Brajmohan <brajmohan@indiamart.com>
2024-02-08 01:03:07 +0530

    refectored APIs code

--- src/appWrite/appwrite.js
+++ src/appWrite/appwrite.js
@@ -1,12 +1,9 @@
-import { Client, Account,Databases, Query} from 'appwrite';
+import { Client, Account,Databases} from 'appwrite';
 
-export const client = new Client();
-
-client
+export const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('656b67273d425a12f953'); // Replace with your project ID
+    .setProject('656b67273d425a12f953');
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 
-export { ID } from 'appwrite';
